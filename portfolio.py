faa608509f5cac9379eed2133fbc08931af0b901
Scott Maxwell <33496996+scottwmwork@users.noreply.github.com>
2020-01-06 00:22:52 -0700

    Changed home.html return placement

--- portfolio.py
+++ portfolio.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>This is my portfolio home page<h1>"
+    return render_template('home.html')
 
 # about page
 @app.route("/about")
 def about():
-    return render_template('home.html')
+    return ""
 
 if __name__ == "__main__":
     app.run()
