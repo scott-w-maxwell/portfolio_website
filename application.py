a4f1858f421b8f113ec10ec2216a396409450763
scottwmwork <scottwmwork@users.noreply.github.com>
2020-01-06 12:24:04 -0700

    Added html template and updated files for deployment on aws

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+application = app = Flask(__name__)
+
+# Basic app output
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('index.html')
+
+# about page
+@app.route("/about")
+def about():
+    return ""
+
+if __name__ == "__main__":
+    app.run()
