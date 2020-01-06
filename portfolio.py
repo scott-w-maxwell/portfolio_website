a43a91803ce77c875f1f84fb43ed5885890e4830
Scott Maxwell <33496996+scottwmwork@users.noreply.github.com>
2020-01-06 00:13:53 -0700

    Added templates folder + about.html & home.html files

--- portfolio.py
+++ portfolio.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 # Basic app output
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "<h1>This is my portfolio<h1>"
+    return "<h1>This is my portfolio home page<h1>"
 
 # about page
 @app.route("/about")
